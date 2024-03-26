e91fc66264ffb46b4770beb0258b73ba4d285545
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-03-26 15:17:59 -0700

    add edit view

---
+++ Library/EditBookView.swift
@@ -0,0 +1,149 @@
+//
+//  EditBookView.swift
+//  Library
+//
+//  Created by özgün aksoy on 2024-03-26.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var dateAdded: Date = Date.distantPast
+    @State private var dateStarted: Date = Date.distantPast
+    @State private var dateCompleted: Date = Date.distantPast
+    @State private var summary: String = ""
+    @State private var rating: Int?
+    @State private var status: Status = .onShelf
+    @State private var firstView: Bool = true
+
+    let book: Book
+    
+    var body: some View {
+        HStack {
+            Text("Status")
+            Picker("Status", selection: $status) {
+                ForEach(Status.allCases) { status in
+                    Text(status.descr).tag(status)
+                }
+            }
+            .buttonStyle(.bordered)
+            .pickerStyle(.segmented)
+        }
+        .padding()
+        VStack(alignment: .leading) {
+            GroupBox {
+                LabeledContent {
+                    DatePicker("", selection: $dateAdded, displayedComponents: .date)
+                } label: {
+                    Text("Date Added")
+                }
+                if status == .inProgress || status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateStarted, in: dateAdded..., displayedComponents: .date)
+                    } label: {
+                        Text("Date Started")
+                    }
+                }
+                if status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateCompleted, in: dateStarted..., displayedComponents: .date)
+                    } label: {
+                        Text("Date Completed")
+                    }
+                }
+            }
+            .foregroundStyle(.secondary)
+            .onChange(of: status) { oldValue, newValue in
+                if !firstView {
+                    if newValue == .onShelf {
+                        dateStarted = .distantPast
+                        dateCompleted = .distantPast
+                    } else if newValue == .inProgress && oldValue == .completed {
+                        dateCompleted = .distantPast // from completed to in progress
+                    } else if newValue == .inProgress && oldValue == .onShelf {
+                        dateStarted = .now // book has been started
+                    } else if newValue == .completed && oldValue == .onShelf {
+                        dateCompleted = .now // forgot to start book
+                        dateStarted = dateAdded
+                    } else {
+                        dateCompleted = .now // completed
+                    }
+                    firstView = false
+                }
+            }
+            Divider()
+            LabeledContent {
+                RatingsView(maxRating: 5, currentRating: $rating)
+            } label: {
+                Text("Rating")
+            }
+            LabeledContent {
+                TextField("", text: $title)
+            } label: {
+                Text("Title").foregroundStyle(.secondary)
+            }
+            LabeledContent {
+                TextField("", text: $author)
+            } label: {
+                Text("Author").foregroundStyle(.secondary)
+            }
+            Divider()
+            Text("Summary").foregroundStyle(.secondary)
+            TextEditor(text: $summary)
+                .padding(5)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20).stroke(.tertiary, lineWidth: 2)
+                }
+
+        }
+        .padding()
+        .textFieldStyle(.roundedBorder)
+        .navigationTitle(title)
+        .toolbar {
+            if changed {
+                Button("Update") {
+                     book.status = status
+                     book.rating = rating
+                     book.title = title
+                     book.author = author
+                     book.summary = summary
+                     book.dateAdded = dateAdded
+                     book.dateStarted = dateStarted
+                     book.dateCompleted = dateCompleted
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+        .onAppear {
+            status = book.status
+            rating = book.rating
+            title = book.title
+            author = book.author
+            summary = book.summary
+            dateAdded = book.dateAdded
+            dateStarted = book.dateStarted
+            dateCompleted = book.dateCompleted
+        }
+    }
+    
+    var changed: Bool {
+        status != book.status
+        || rating != book.rating
+        || title != book.title
+        || author != book.author
+        || summary != book.summary
+        || dateAdded != book.dateAdded
+        || dateStarted != book.dateStarted
+        || dateCompleted != book.dateCompleted
+    }
+}
+
+#Preview {
+    NavigationStack {
+        EditBookView(book: Book(title: "asd", author: "123"))
+    }
+}
