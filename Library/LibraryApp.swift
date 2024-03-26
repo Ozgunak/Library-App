786f12d57b7696f8ac74fa4fbda24ec229867d39
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-03-26 12:44:30 -0700

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,32 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by özgün aksoy on 2024-03-26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LibraryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
